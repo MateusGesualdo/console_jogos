9bc0db8a517e1c24f08291580d964804fc340c03
MateusGesualdo <57274184+MateusGesualdo@users.noreply.github.com>
2024-09-09 19:59:29 -0300

    adicionei os jogos criados no Colab

---
+++ quiz.py
@@ -0,0 +1,11 @@
+def quiz():
+  pergunta = "Qual é a capital do Brasil?"
+  resposta_correta = "Brasília"
+
+  print(pergunta)
+  resposta = input("Sua resposta: ")
+
+  if resposta.lower() == resposta_correta.lower():
+      print("Correto!")
+  else:
+      print(f"Errado! A resposta correta é {resposta_correta}")  
